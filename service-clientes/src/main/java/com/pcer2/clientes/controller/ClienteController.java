ca44cfb05b68ff5e939d851bb0db132180223d04
fmoyam <fel.moyam@duocuc.cl>
2026-05-11 14:28:07 -0400

    Solo se cambio el nombre de la carpeta del microservicio

--- service-clientes/src/main/java/com/pcer2/clientes/controller/ClienteController.java
+++ service-clientes/src/main/java/com/pcer2/clientes/controller/ClienteController.java
@@ -1,8 +1,6 @@
 package com.pcer2.clientes.controller;
-
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.pcer2.clientes.model.Cliente;
 import com.pcer2.clientes.service.ClienteService;
 
