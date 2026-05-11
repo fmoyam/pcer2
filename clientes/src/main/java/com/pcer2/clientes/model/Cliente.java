c47adf0cb0320d6d7f2c7be5ba63a537d56c0f5f
fmoyam <fel.moyam@duocuc.cl>
2026-05-11 11:01:51 -0400

    Se crearon los paquetes modelo-servicio, además de la clase 'Cliente'.

---
+++ clientes/src/main/java/com/pcer2/clientes/model/Cliente.java
@@ -0,0 +1,25 @@
+package com.pcer2.clientes.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String rut;
+    private int telefono;
+    private String email;
+    private LocalDate fecha_registro;
+    private int ordenes_totales;
+
+}
