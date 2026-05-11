ddf6392412c2af9b701ba15bc9c888996646d87f
fmoyam <fel.moyam@duocuc.cl>
2026-05-11 14:47:34 -0400

    k

--- service-clientes/src/main/java/com/pcer2/clientes/model/Cliente.java
+++ service-clientes/src/main/java/com/pcer2/clientes/model/Cliente.java
@@ -1,7 +1,5 @@
 package com.pcer2.clientes.model;
-
 import java.time.LocalDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
