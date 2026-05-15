8f362e6195f74155bf13064c2c32c38949388536
Sarai-Alejandra <sar.jarac@duocuc.cl>
2026-05-15 01:06:19 -0400

    Agrega api-gateway y service-voucher

---
+++ api-gateway/api_getawey/src/main/java/com/pcer2/api_getawey/ApiGetaweyApplication.java
@@ -0,0 +1,13 @@
+package com.pcer2.api_getawey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetaweyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetaweyApplication.class, args);
+	}
+
+}
