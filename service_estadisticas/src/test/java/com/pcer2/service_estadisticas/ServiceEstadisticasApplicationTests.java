bd59a3c3961e09c493c65f08eb38cb2e34b13bb2
fmoyam <fel.moyam@duocuc.cl>
2026-06-12 12:24:39 -0400

    Se añaden carpetas de nuevos microservicios

---
+++ service_estadisticas/src/test/java/com/pcer2/service_estadisticas/ServiceEstadisticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcer2.service_estadisticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEstadisticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service_hardware/src/test/java/com/pcer2/service_hardware/ServiceHardwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcer2.service_hardware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHardwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
