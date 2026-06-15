113d38bbc96634a28fb7f86cfb34e9ab02a377b8
fmoyam <fel.moyam@duocuc.cl>
2026-06-15 12:30:40 -0400

    Se eliminaron todas las referencias a 'tipoEquipo' y 'tipoAlmacen'. El modelo de equipo reemplazó por strings ambos atributos y por consiguiente, se eliminaron lo que antes eran modelos, controladores y servicios relacionados.

--- service-equipo/src/main/java/com/pcer2/service_equipo/dto/EquipoDTO.java
+++ service-equipo/src/main/java/com/pcer2/service_equipo/dto/EquipoDTO.java
@@ -9,14 +9,14 @@ import lombok.NoArgsConstructor;
 public class EquipoDTO {
     private Long id;
     private Long clienteId;  // Referencia al cliente
-    private TipoEquipoDTO tipoEquipo;
+    private String tipoEquipo;
     private String marca;
     private String modelo_cpu;
     private String placa_madre;
     private int ram_cantidad;
     private int ram_frecuencia;
     private int almacen_cantidad;
-    private TipoAlmacenDTO almacen_tipo;
+    private String tipoAlmacen;
     private String numeroserie;
     private int veces_reparado;
 }
--- service-equipo/src/main/java/com/pcer2/service_equipo/model/Equipo.java
+++ service-equipo/src/main/java/com/pcer2/service_equipo/model/Equipo.java
@@ -1,15 +1,10 @@
 package com.pcer2.service_equipo.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,22 +19,14 @@ public class Equipo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "tipo_equipo_id")
-    @JsonIgnoreProperties("equipos")
-    private TipoEquipo tipoEquipo;
-
+    private String tipoEquipo;  // pc, notebook, minipc, server, etc.
     private String marca;
     private String modelo_cpu;
     private String placa_madre;
     private int ram_cantidad;
     private int ram_frecuencia;
-    private int almacen_cantidad;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "tipo_almacen_id")
-    @JsonIgnoreProperties("equipos")    
-    private TipoAlmacen almacen_tipo;
+    private int almacen_cantidad;  
+    private String tipoAlmacen; // ssd, hdd, microSD, emmc, etc.
     
     @Column(unique = true)
     private String numeroserie;
