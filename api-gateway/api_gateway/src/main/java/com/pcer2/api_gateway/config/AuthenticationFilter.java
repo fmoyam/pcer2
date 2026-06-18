548f5cb80922a24b60e39e5e98ad8a79bd400af6
Sarai-Alejandra <sar.jarac@duocuc.cl>
2026-06-18 19:17:58 -0400

    se modifica api

---
+++ api-gateway/api_gateway/src/main/java/com/pcer2/api_gateway/config/AuthenticationFilter.java
@@ -0,0 +1,63 @@
+package com.pcer2.api_gateway.config;
+
+import java.nio.charset.StandardCharsets;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config>{
+
+    @Value("${jwt.secret:493358340235739058340598340598340598340598340598}")
+    private String secreto;
+    public AuthenticationFilter(){
+        super(Config.class);
+    }
+    public static class Config{
+        private String name;
+
+        public Config() {}
+
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+        //configuracion adicional si fuera necesaria
+    }
+    @Override
+    public GatewayFilter apply(Config config)
+    {
+        return (exchange, chain) ->
+        {
+            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION); 
+            if (authHeader == null || !authHeader.startsWith("Bearer"))
+            {
+                return onError(exchange, "Token faltante o formato invalido", HttpStatus.UNAUTHORIZED);
+            }
+            String token = authHeader.substring(7);
+            try{
+                Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secreto.getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseClaimsJws(token);
+            }catch (Exception e)
+            {
+                return onError(exchange, "token invalido o expirado", HttpStatus.UNAUTHORIZED);
+            }
+            return chain.filter(exchange);
+        };
+    }
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus)
+    {
+        exchange.getResponse().setStatusCode(httpStatus);
+        return exchange.getResponse().setComplete();
+    }
+
+}
